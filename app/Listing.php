ff6ae8d5dd07bbe3915d0b2dc3a83c308ad6e716
ryu-fuji <ryu.techacademia@gmail.com>
2019-01-05 09:03:29 +0000

    kanban project

---
+++ app/Listing.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    public function cards() {
+        return $this->hasMany('App\Card');
+    }
+}
